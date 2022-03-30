9f468cd3ceb0b4490011b7d007d2f35c7754bdba
Songpol Siengvisuth <songpol.siengvisuth@slalom.com>
2022-03-30 12:48:08 -0700

    Initail Commit

---
+++ src/main/java/com/learn/springbootkubernetes/Controller.java
@@ -0,0 +1,12 @@
+package com.learn.springbootkubernetes;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Spring-Boot app successfully deployed and running on ROSA";
+    }
+}
