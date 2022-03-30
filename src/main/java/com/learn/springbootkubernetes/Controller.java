115c5c48464bb3a8f7dc3eb97a043b2d85bb9bca
Songpol Siengvisuth <songpol.siengvisuth@slalom.com>
2022-03-30 12:58:22 -0700

    Modfied message.

--- src/main/java/com/learn/springbootkubernetes/Controller.java
+++ src/main/java/com/learn/springbootkubernetes/Controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @RequestMapping("/")
     public String index() {
-        return "Hello Spring-Boot app successfully deployed and running on ROSA";
+        return "Hello Spring-Boot app successfully deployed and running on ROSA -- Modified";
     }
 }
